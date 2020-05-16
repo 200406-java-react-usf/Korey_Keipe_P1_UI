2abca49f19372d9f77435cb9e1d6154be6a4677d
khkeipe <kkeipe@gmail.com>
2020-05-16 18:20:40 -0500

    changing button position

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { getUsers, logOut } from '../remote/user-service';
-import { Button } from '@material-ui/core'
+import { Button, makeStyles } from '@material-ui/core'
 import { User } from '../models/user';
 
 interface IHomeProps {
@@ -10,22 +10,41 @@ interface IHomeProps {
     setAuthUser: (user: User) => void;    
 }
 
+const useStyles = makeStyles({
+    Container: {
+        display: "flex",
+        justifyContent: "center",
+        margin: 20,
+        marginTop: 40,
+        padding: 20
+    },
+    Form: {
+        width: "50%"
+    }
+});
+
 const HomeComponent = (props: IHomeProps) => {
 	
+	const classes = useStyles();
+
 	let logout = async () => {
 		await logOut();
 		//@ts-ignore
 		props.setAuthUser(null);
+		console.log('Logged out');
+		
 	}
 	return (
 		!props.username ?
 		<Redirect to="/login" /> :
 		<>
-			<h1> Welcome, {props.username}!</h1>
-			<Button onClick={getUsers} variant="contained" color="primary" size="medium">Get Users</Button>
-			<br/>
-			<Button onClick={logout} variant="contained" color="primary" size="medium">Log Out</Button>
-
+			<div className={classes.Container}>
+				<h1> Welcome, {props.username}!</h1>
+					<br/><br/>
+					<Button onClick={getUsers} variant="contained" color="primary" size="medium">Get Users</Button>
+					<br/><br/>
+					<Button onClick={logout} variant="contained" color="primary" size="medium">Log Out</Button>
+			</div>
 		</>	
 	);
 }
