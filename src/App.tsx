14f2372d06e6f055ee03837f6dd0e5fe21c49ad0
khkeipe <kkeipe@gmail.com>
2020-05-16 17:37:41 -0500

    reimplement lost items

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
 
 import NavbarComponent from './components/NavBarComponent';
+import HomeComponent from './components/HomeComponent';
+import LoginComponent from './components/LoginComponent';
+import RegisterComponent from './components/RegisterComponent';
 
 function App() {
   
   // @ts-ignore
   const [authUser, setAuthUser] = useState(null as User);
+  // @ts-ignore
+  const [newUser, setNewUser] = useState(null as User);
+  const [errorMessage, setErrorMessage] = useState('');
   
   return (
     
@@ -23,7 +29,9 @@ function App() {
         </AppBar>
 
         <Switch>
-          <Route></Route>          
+          <Route path="/home" render={() => <HomeComponent authUser={authUser} username={authUser?.username} setAuthUser={setAuthUser} /> } />          
+          <Route path="/login" render={() => <LoginComponent authUser={authUser} setAuthUser={setAuthUser} /> } />          
+          <Route path="/register" render={() => <RegisterComponent authUser={authUser} setNewUser={setNewUser} errorMessage={errorMessage} /> } />          
 
         </Switch>
       </Router>
