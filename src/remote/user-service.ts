65f6a952d428725ec3570ef34a45ea07c0a7ce58
khkeipe <kkeipe@gmail.com>
2020-05-16 18:21:44 -0500

    adding register method

--- src/remote/user-service.ts
+++ src/remote/user-service.ts
@@ -1,5 +1,6 @@
 import { userClient } from './client';
 import { User } from '../models/user';
+import { NewUser } from '../models/newUser';
 
 export const getUsers = async () => {
 
@@ -12,7 +13,7 @@ export const logOut = async () => {
 	await userClient.get('/auth');
 }
 
-export async function registerUser(newUser: User) {
+export async function registerUser(newUser: NewUser) {
 
 	let response = await userClient.post('/users', {newUser});
 		console.log(response.data);
