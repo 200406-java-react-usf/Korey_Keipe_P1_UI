784cd54eadee2dd666ccafb2ec94c0050c0d6b37
khkeipe <kkeipe@gmail.com>
2020-05-17 15:33:51 -0500

    change input perams for newUser

--- src/remote/user-service.ts
+++ src/remote/user-service.ts
@@ -1,19 +1,26 @@
 import { userClient } from './client';
+import { localClient } from './client';
 import { NewUser } from '../models/newUser';
 
 export const getUsers = async () => {
 
-	let users = await userClient.get('/users');
+	let users = await localClient.get('/users');
 	return users.data;	
 }
 
 export const logOut = async () => {
-	await userClient.get('/auth');
+	await localClient.get('/auth');
 }
 
 export async function registerUser(newUser: NewUser) {
-
-	let response = await userClient.post('/users', {newUser});
-		console.log(response.data);
+	
+	let response = await localClient.post('/users', 
+		{username: newUser.username,
+		password: newUser.password, 
+		firstName: newUser.firstName, 
+		lastName: newUser.lastName, 
+		email: newUser.email, 
+		role_id: newUser.role_id
+		});
 	return await response.data;
 }
