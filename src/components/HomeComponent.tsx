10a879ccf3b9b74d9ba3cf70b710203ed9b1188b
khkeipe <kkeipe@gmail.com>
2020-05-17 15:37:44 -0500

    removing logout logic to move to navbar

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { getUsers, logOut } from '../remote/user-service';
-import { Button, makeStyles } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core'
 import { User } from '../models/user';
 import { Redirect } from 'react-router-dom';
 
@@ -27,26 +26,12 @@ const HomeComponent = (props: IHomeProps) => {
 	
 	const classes = useStyles();
 
-	let logout = async () => {
-		await logOut();
-		//@ts-ignore
-		props.setAuthUser(null);
-		console.log('Logged out');
-	}
-
 	return (
-		// !props.username ?
-		// <Redirect to="/login" /> :
+		!props.username ?
+		<Redirect to="/login" /> :
 		<>
 			<div className={classes.Container}>
 				<h1> Welcome, {props.username}!</h1>
-					<br/><br/>
-						<Button onClick={() => <Redirect to={"/users"} />} variant="contained" color="primary" size="medium">Get Users</Button>
-					<br/><br/>
-					<p>
-						
-					</p>
-					<Button onClick={logout} variant="contained" color="primary" size="medium">Log Out</Button>
 			</div>
 		</>	
 	);
