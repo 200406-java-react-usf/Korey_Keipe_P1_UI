66f0fbe45f07aa21ecf2f09e927bb4d3beb15165
khkeipe <kkeipe@gmail.com>
2020-05-16 21:35:24 -0500

    adding user comp route

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import NavbarComponent from './components/NavBarComponent';
 import HomeComponent from './components/HomeComponent';
 import LoginComponent from './components/LoginComponent';
 import RegisterComponent from './components/RegisterComponent';
+import UserComponent from './components/UserComponent';
 
 function App() {
   
@@ -20,19 +21,19 @@ function App() {
     <>
       <Router>
 
-        <AppBar color="primary" position="fixed">
+        <AppBar color="primary" position="static">
           <Toolbar>
             <Typography>
                 <NavbarComponent authUser={authUser}/>
             </Typography>
           </Toolbar>
         </AppBar>
-
+        
         <Switch>
           <Route path="/home" render={() => <HomeComponent authUser={authUser} username={authUser?.username} setAuthUser={setAuthUser} /> } />          
           <Route path="/login" render={() => <LoginComponent authUser={authUser} setAuthUser={setAuthUser} /> } />          
-          <Route path="/register" render={() => <RegisterComponent authUser={authUser} setNewUser={setNewUser} errorMessage={errorMessage} /> } />          
-
+          <Route path="/register" render={() => <RegisterComponent authUser={authUser} setNewUser={setNewUser} errorMessage={errorMessage} /> } /> 
+          <Route path="/users" render={() => <UserComponent authUser={authUser} /> } />        
         </Switch>
       </Router>
     </>
