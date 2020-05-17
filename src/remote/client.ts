0528366ebdefd71cea3ed99b56abf325e0441c2a
khkeipe <kkeipe@gmail.com>
2020-05-17 15:32:11 -0500

    adding withCredentials

--- src/remote/client.ts
+++ src/remote/client.ts
@@ -5,5 +5,14 @@ export const userClient = axios.create({
     baseURL: 'http://KhkProject1Api-env.eba-pamzru3a.us-east-1.elasticbeanstalk.com',
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
+});
+
+export const localClient = axios.create({
+    baseURL: 'http://localhost:8080',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    withCredentials: true
 });
