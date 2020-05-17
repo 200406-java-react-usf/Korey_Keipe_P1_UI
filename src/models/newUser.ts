bc5223837845bb20558240ce743c568a8019bd96
khkeipe <kkeipe@gmail.com>
2020-05-17 15:33:07 -0500

    changing role_id to type number

--- src/models/newUser.ts
+++ src/models/newUser.ts
@@ -5,15 +5,15 @@ export class NewUser {
 	firstName: string;
     lastName: string;
 	email: string;
-	role: string;   
+	role_id: number;   
     
-    constructor(username: string, password: string, firstName: string, lastName: string, email: string, role: string) {
+    constructor(username: string, password: string, firstName: string, lastName: string, email: string, role_id: number) {
 		
 		this.username = username;
 		this.password = password;
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.email = email;
-		this.role = role;
+		this.role_id = role_id;
     }
 }
