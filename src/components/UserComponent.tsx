fa151ac77237f6359a44be6c2ebdbdcde3aaeb8c
khkeipe <kkeipe@gmail.com>
2020-05-18 18:57:22 -0500

    create user cards

--- src/components/UserComponent.tsx
+++ src/components/UserComponent.tsx
@@ -1,13 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { User } from '../models/user';
 import { getUsers } from '../remote/user-service';
+import { Card, CardContent, Typography, makeStyles } from '@material-ui/core';
 
 interface IUserProps{
     authUser: User;
 }
+const useStyles = makeStyles({
+    root: {
+      minWidth: 275,
+      maxWidth: '50%',
+      margin: 'auto',
+      textAlign: 'center',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    bullet: {
+      display: 'flex',
+      margin: '0 2px',
+      transform: 'scale(0.8)',
+    },
+    title: {
+      fontSize: 24,
+    },
+    pos: {
+      marginBottom: 12,
+    },
+    id: {
+        textAlign: 'left'
+    }
+  });
 
 const UserComponent = (props: IUserProps) => {
     
+    const classes = useStyles();
+
     const [usersState, setUsersState] = useState([] as User[]);
 
     let users: any[] = [];
@@ -19,23 +46,31 @@ const UserComponent = (props: IUserProps) => {
 				
 			for(let user of response) {
 				users.push(
-					<tr>
-                        <td>{user.user_id}</td>
-                        <td>{user.first_name}</td>
-                        <td>{user.last_name}</td>
-                        <td>{user.username}</td>
-                        <td>{user.email}</td>
 
-                        {
-                        user.role_id === 1 ?
-                            <td>Admin</td> 
-                            :
-                        user.role_id === 2 ?
-                            <td>Financial Manager</td>
-                            :
-                            <td>User</td>
-                        }
-                    </tr>
+                    <Card className={classes.root} variant="outlined">
+                        <CardContent>
+                            <Typography className={classes.title} variant="h6" color="textPrimary" gutterBottom>
+                                {user.first_name} {user.last_name} {'# ' + user.user_id}
+                            </Typography>
+                            
+                            <Typography className={classes.pos} color="textSecondary">
+                                {'Username: ' + user.username}
+                                <br/>
+                                {'Email: ' + user.email}
+                            </Typography>
+                            <Typography variant="body2" component="p">
+                                {
+                                    user.role_id === 1 ?
+                                    <td>Role: Admin</td> 
+                                    :
+                                    user.role_id === 2 ?
+                                    <td>Role: Financial Manager</td>
+                                    :
+                                    <td>Role: User</td>
+                                }
+                            </Typography>
+                        </CardContent>
+                    </Card>
 				)
 			}
 			setUsersState(users);
@@ -45,31 +80,8 @@ const UserComponent = (props: IUserProps) => {
 	},[]);
 
     return (
-		!props.authUser || (props.authUser.role_id !== 1) ?
-        
         <>
-            <h1>Youre not authorized to view this page</h1>
-        </>
-        :
-        <>
-            <h1>User Component</h1>
-            
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Username</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                    </tr>
-                </thead>
-
-                <tbody>
-                    {usersState}
-                </tbody>
-            </table>
+            {usersState}
         </>
     );
 }
