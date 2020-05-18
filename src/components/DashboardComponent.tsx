cebd418602534a8c4acc2abc2ca440d374908e51
khkeipe <kkeipe@gmail.com>
2020-05-18 10:57:03 -0500

    started adaptive dashboard based on role_id

--- src/components/DashboardComponent.tsx
+++ src/components/DashboardComponent.tsx
@@ -1,31 +1,28 @@
 import React from 'react';
 import { User } from "../models/user";
-import { Typography, makeStyles } from '@material-ui/core';
+import { Typography, makeStyles, Grid, Theme, createStyles, Paper } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 
 interface IDashboardProps {
 	authUser: User;
 }
 
-const useStyles = makeStyles({
-    Container: {
-        display: "flex",
-        justifyContent: "center",
-        margin: 20,
-        marginTop: 40,
-        padding: 20
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
     },
-    Form: {
-        width: "50%"
-	},
-	title: {
-		flexGrow: 1,
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+      color: theme.palette.text.secondary,
 	},
 	link: {
         textDecoration: 'none',
         color: 'white'
     }
-});
+  }),
+);
 
 function DashboardComponent(props: IDashboardProps) {
 
@@ -34,13 +31,49 @@ function DashboardComponent(props: IDashboardProps) {
 	return (
 		
 		<>
-			<Typography variant="h6" className={classes.title}>
-						{ props.authUser ? <Link to="/users" className={classes.link}>Get Users</Link> : <></> }
-			</Typography>
+				<div className={classes.root}>
+					{props.authUser.role_id === 1 ?
+					<Grid container spacing={3}>
+						<Grid item xs>
+							<Paper className={classes.paper}><Link to="/users" className={classes.link}>Get Users</Link></Paper>
+						</Grid>
+						<Grid item xs>
+							<Paper className={classes.paper}><Link to="/register" className={classes.link}>Register</Link></Paper>
+						</Grid>
+						<Grid item xs>
+							<Paper className={classes.paper}>xs</Paper>
+						</Grid>
+					</Grid>
+					: <></> }
+					{props.authUser.role_id === 2 ?
+					<Grid container spacing={3}>
+						<Grid item xs>
+							<Paper className={classes.paper}>Manger Stuff</Paper>
+						</Grid>
+						<Grid item xs={6}>
+							<Paper className={classes.paper}>xs=6</Paper>
+						</Grid>
+						<Grid item xs>
+							<Paper className={classes.paper}>xs</Paper>
+						</Grid>
+					</Grid>
+					: <></> }
+					{props.authUser.role_id === 3 ?
+					<Grid container spacing={3}>
+						<Grid item xs>
+							<Paper className={classes.paper}>User Stuff</Paper>
+						</Grid>
+						<Grid item xs={6}>
+							<Paper className={classes.paper}>xs=6</Paper>
+						</Grid>
+						<Grid item xs>
+							<Paper className={classes.paper}>xs</Paper>
+						</Grid>
+					</Grid>
+					: <></> }
 
-			<Typography variant="h6" className={classes.title}>
-						{ props.authUser ? <Link to="/register" className={classes.link}>Register</Link> :	<></> }
-			</Typography>
+				</div>
+
 		</>
 	);
 }
