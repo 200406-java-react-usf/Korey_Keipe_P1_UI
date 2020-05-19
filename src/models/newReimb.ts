cb781caa1b74218c508a704c96487f2fe588952e
khkeipe <kkeipe@gmail.com>
2020-05-19 10:19:32 -0500

    modify newReimb model :

--- src/models/newReimb.ts
+++ src/models/newReimb.ts
@@ -2,12 +2,14 @@ export class NewReimb{
 
 	amount: number;
 	description: string;
+	author_id: number | null;
 	type_id: number;
 
-	constructor(amt: number, desc: string, type: number){
+	constructor(amt: number, desc: string, author_id: number | null, type_id: number){
 
 		this.amount = amt;
 		this.description = desc;
-		this.type_id = type;
+		this.author_id = author_id;
+		this.type_id = type_id;
 	}
 }
