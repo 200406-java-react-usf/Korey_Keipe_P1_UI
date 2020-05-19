cc5d865792cfe3650a65682edb20bdcaf39a14e3
khkeipe <kkeipe@gmail.com>
2020-05-19 10:19:09 -0500

    corrected spelling mistake

--- src/remote/reimb-service.ts
+++ src/remote/reimb-service.ts
@@ -12,12 +12,13 @@ export const getReimbs = async () => {
 
 export const createReimb = async (newReimb: NewReimb) => {
 
-	let response = await currentClient.post('/reimbursement', {
-		
+	let response = await currentClient.post('/reimbursements', 
+	{
 		amount: newReimb.amount,
 		description: newReimb.description,
+		author_id: newReimb.author_id,
 		type_id: newReimb.type_id
 	});
-	return response;
+	return response.data;
 }
 
