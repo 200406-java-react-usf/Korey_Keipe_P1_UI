01a5ce7936a5be7446bc216c72834944c5fed419
khkeipe <kkeipe@gmail.com>
2020-05-18 23:54:42 -0500

    move getReimb to reimb service

--- src/remote/user-service.ts
+++ src/remote/user-service.ts
@@ -1,20 +1,22 @@
-// import { userClient } from './client';
+import { remoteClient } from './client';
 import { localClient } from './client';
 import { NewUser } from '../models/newUser';
 
+let currentClient = localClient;
+
 export const getUsers = async () => {
 
-	let users = await localClient.get('/users');
+	let users = await currentClient.get('/users');
 	return users.data;	
 }
 
 export const logOut = async () => {
-	await localClient.get('/auth');
+	await currentClient.get('/auth');
 }
 
 export async function registerUser(newUser: NewUser) {
 	
-	let response = await localClient.post('/users', 
+	let response = await currentClient.post('/users', 
 		{username: newUser.username,
 		password: newUser.password, 
 		firstName: newUser.firstName, 
@@ -25,8 +27,21 @@ export async function registerUser(newUser: NewUser) {
 	return await response.data;
 }
 
-export const getReimbs = async () => {
+export const updateUser = async (updatedUser: NewUser) => {
 
-	let reimbs = await localClient.get('/reimbursements');
-	return reimbs.data;
+	let response = await currentClient.put('/users', 
+		{username: updatedUser.username,
+		password: updatedUser.password, 
+		firstName: updatedUser.firstName, 
+		lastName: updatedUser.lastName, 
+		email: updatedUser.email, 
+		role_id: updatedUser.role_id
+		});
+	return await response.data;	
+}
+
+export const getUserById = async (user_id: number) => {
+
+	let response = await currentClient.get(`/users/${user_id}`)
+	return response.data;
 }
