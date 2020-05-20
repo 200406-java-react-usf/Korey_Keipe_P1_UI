96f42fcbcc562f6d00bb97e99069c72e01889b8b
khkeipe <kkeipe@gmail.com>
2020-05-19 20:49:19 -0500

    adding update request for reimb

--- src/remote/reimb-service.ts
+++ src/remote/reimb-service.ts
@@ -1,6 +1,7 @@
 import { remoteClient } from './client';
 import { localClient } from './client';
 import { NewReimb } from '../models/newReimb';
+import { Reimb } from '../models/reimb';
 
 let currentClient = localClient;
 
@@ -18,6 +19,29 @@ export const createReimb = async (newReimb: NewReimb) => {
 		description: newReimb.description,
 		author_id: newReimb.author_id,
 		type_id: newReimb.type_id
+	});
+	return response.data;
+}
+
+export const getReimbById = async (id: number) => {
+
+	let response = await currentClient.get(`/reimbursements/${id}`)
+	return response.data;
+}
+
+export const updateReimb = async (updatedReimb: Reimb) => {
+	
+	let response = await currentClient.put('/reimbursements', 
+	{
+		id: updatedReimb.id,
+		amount: updatedReimb.amount,
+		submitted: updatedReimb.submitted,
+		resolved: updatedReimb.resolved,
+		description: updatedReimb.description,
+		author_id: updatedReimb.author_id,
+		resolver_id: updatedReimb.resolver_id,
+		status_id: updatedReimb.status_id,
+		type_id: updatedReimb.type_id
 	});
 	return response.data;
 }
