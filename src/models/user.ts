ee3e26740b1c452d63a4463f1ec627705cde43c4
khkeipe <kkeipe@gmail.com>
2020-05-20 10:59:03 -0500

    remove or null from user id

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    user_id: number | null;
+    user_id: number;
     username: string;
     password: string;
     first_name: string;
@@ -7,7 +7,7 @@ export class User {
 	email: string;
 	role_id: number;
 
-    constructor(user_id: number | null, un: string, pw: string, fn: string, ln: string, email: string, role_id: number) {
+    constructor(user_id: number, un: string, pw: string, fn: string, ln: string, email: string, role_id: number) {
         this.user_id = user_id;
         this.username = un;
         this.password = pw;
